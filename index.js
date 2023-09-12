e31fb8809e2683e0c0331acb859e21a8b5784a83
Martina Baez <Martiibaez2015@hotmail.com>
2023-09-11 22:38:48 -0300

    E-4

---
+++ index.js
@@ -0,0 +1,65 @@
+const idPokemon = document.getElementById('idPokemon')
+const boton = document.getElementById('botonBusqueda')
+const container = document.getElementById('pokeContainer')
+
+const requestPokemon = async (id) => {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+        const data = await response.json()
+        pokemonData(data)
+        return data;
+    } catch (error) {
+        console.log('error', error)
+    }
+}
+
+
+const mostrarPokemon = async () => {
+    const id = idPokemon.value;
+    if (id) {
+        const data = await requestPokemon(id);
+        if (data) {
+
+            pokemonData(data);
+        } else {
+            container.innerHTML = '<p>No existe ese Pokemón</p>';
+        }
+    }
+}
+
+const pokemonData = (data) => {
+    container.innerHTML = ''
+    const pokemon = data.name
+    const tipo = data.types[0].type.name;
+    const altura = data.height
+    const peso = data.weight
+    const imagen = data.sprites.front_default
+
+
+    const pokemonNombre = document.createElement("h1")
+    pokemonNombre.textContent = pokemon
+
+    const types = document.createElement("h2")
+    types.textContent = `El tipo de ${pokemon} es : ${tipo} `
+
+    const alturaPokemon = document.createElement("p")
+    alturaPokemon.textContent = `La altura de ${pokemon} es ${altura / 10} de metros`
+
+    const pesoPokemon = document.createElement("p")
+    pesoPokemon.textContent = `El peso de ${pokemon} es ${peso / 10} kilogramos`
+
+    const fotoPokemon = document.createElement("img")
+    fotoPokemon.src = imagen
+
+    container.appendChild(pokemonNombre)
+    container.appendChild(types)
+    container.appendChild(alturaPokemon)
+    container.appendChild(pesoPokemon)
+    container.appendChild(fotoPokemon)
+
+}
+
+const init = () => {
+    boton.addEventListener("click", mostrarPokemon)
+}
+init()
